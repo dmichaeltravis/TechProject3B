acec46264d85e291a59ce327453e33da2578860d
dmichaeltravis@hotmail.com <dmichaeltravis@hotmail.com>
2020-09-23 13:15:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TechProject3B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int [25];
+            // Store array values
+            for (int index1 = 0; index1 < 25; index1++)
+            {
+                intArray[index1] = 2 * index1;
+            }
+            // Display Array values at Console
+            for (int index1 = 0; index1 < 25; index1++)
+            {
+                Console.WriteLine("Array value " + index1 + " Equal to " + intArray[index1]);
+            }
+        }
+    }
+}
